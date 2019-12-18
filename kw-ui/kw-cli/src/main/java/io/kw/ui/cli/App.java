e3d8e8a3910189de089ad5f55fbbd7ba0ce737fd
Keyshawn Butts <kjbappsllc@gmail.com>
2019-12-18 00:49:16 -0500

    switched to using quarkus

--- kw-ui/kw-cli/src/main/java/io/kw/ui/cli/App.java
+++ kw-ui/kw-cli/src/main/java/io/kw/ui/cli/App.java
@@ -4,14 +4,12 @@ import io.kw.engine.core.PriceStreamingEngine;
 import io.kw.model.bar.Timeframe;
 import io.kw.model.currency.Currency;
 import io.kw.model.currency.CurrencyPair;
-import org.jboss.weld.environment.se.bindings.Parameters;
-import org.jboss.weld.environment.se.events.ContainerInitialized;
+import io.quarkus.runtime.StartupEvent;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import java.math.BigDecimal;
-import java.util.List;
 
 @ApplicationScoped
 public class App {
@@ -19,16 +17,15 @@ public class App {
     @Inject
     PriceStreamingEngine priceStreamingEngine;
 
-    void onAppStart(@Observes ContainerInitialized event,
-                    @Parameters List<String> args) {
-        System.out.println("Working: " + args);
-//        priceStreamingEngine.startPricingStream(
-//                new CurrencyPair(
-//                        Currency.EUR,
-//                        Currency.USD,
-//                        new BigDecimal("0.2")
-//                ),
-//                Timeframe.M5
-//        );
+    void onAppStart(@Observes StartupEvent event) {
+        System.out.println("Working: ");
+        priceStreamingEngine.startPricingStream(
+                new CurrencyPair(
+                        Currency.EUR,
+                        Currency.USD,
+                        new BigDecimal("0.2")
+                ),
+                Timeframe.M5
+        );
     }
 }
